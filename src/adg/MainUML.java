70d0996d96de038a7a402e2a7ea7ed003be89c08
Valentxn7 <gugug3293@gmail.com>
2024-12-12 14:42:28 +0100

    camelCase + faute ortho

--- src/adg/MainUML.java
+++ src/adg/MainUML.java
@@ -5,8 +5,6 @@ import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
