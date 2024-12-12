15a1113291d7972a20b61315d33695022e08cbd4
Geoffrey2501 <132345682+Geoffrey2501@users.noreply.github.com>
2024-12-12 09:39:03 +0100

    Create VueClasse.java

---
+++ src/adg/VueClasse.java
@@ -0,0 +1,12 @@
+package adg;
+
+public class VueClasse implements Vue{
+    private Classe classe;
+    public VueClasse(Classe classe){
+        this.classe = classe;
+    }
+    @Override
+    public void actualiser(Sujet mod) {
+        
+    }
+}
