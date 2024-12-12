d52834a5d2055a1e5682479af437ba4975ed2ddc
SolareFlame <zeldac332@gmail.com>
2024-12-12 14:24:55 +0100

    🚧 Temporary Main to test Save UML Option

---
+++ src/adg/MainSave.java
@@ -0,0 +1,30 @@
+package adg;
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainSave {
+
+    public static void main(String[] args) throws ClassNotFoundException {
+        File directory = new File("src/adg");
+        FilenameFilter filter = (dir, name) -> name.endsWith(".java");
+        File[] javaFiles = directory.listFiles(filter);
+
+        if (javaFiles != null) {
+            List<Classe> classes = new ArrayList<>();
+
+            for(File file : javaFiles) {
+                String className = "adg." + file.getName().substring(0, file.getName().length() - 5);
+                Classe classe = new Classe(className);
+
+                classes.add(classe);
+            }
+
+            Save save = Save.getInstance(classes, null);
+            save.saveUML();
+        } else {
+            System.out.println("Aucun fichier .java trouvé dans le répertoire spécifié.");
+        }
+    }
+}
