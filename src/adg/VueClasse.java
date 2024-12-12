ea1e7c2b6827f2751e0f2c98acccd0072c4fe713
Valentxn7 <gugug3293@gmail.com>
2024-12-12 15:50:53 +0100

    idk

--- src/adg/VueClasse.java
+++ src/adg/VueClasse.java
@@ -6,22 +6,25 @@ import java.util.ArrayList;
 
 public class VueClasse extends Label implements Vue{
     private Classe classe;
+
     public VueClasse(Classe classe){
         this.classe = classe;
         this.setId("classe-label");
     }
+
     @Override
     public void actualiser(Sujet mod) {
         ModelUML m = (ModelUML) mod;
         String text = classe.getNom()+"\n";
         ArrayList attributs = classe.getAttributs();
         ArrayList methodes = classe.getMethodes();
-        for(int i = 0; i < attributs.size(); i++){
-            text+=attributs.get(i)+"\n";
-        }
-        for(int i = 0; i < methodes.size(); i++){
-            text+=methodes.get(i)+"\n";
-        }
+
+        for(int i = 0; i < attributs.size(); i++)
+            text += attributs.get(i)+"\n";
+
+        for(int i = 0; i < methodes.size(); i++)
+            text += methodes.get(i)+"\n";
+
         this.setText(text);
     }
 }
