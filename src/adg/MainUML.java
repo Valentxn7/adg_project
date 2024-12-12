0d6a86e2611bd50cb7a2e8cc1b770a1a9ab50521
Geoffrey2501 <132345682+Geoffrey2501@users.noreply.github.com>
2024-12-12 11:06:13 +0100

    ajout de la partie affichage

--- src/adg/MainUML.java
+++ src/adg/MainUML.java
@@ -2,6 +2,7 @@ package adg;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -12,8 +13,19 @@ public class MainUML extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+        Classe maClasse = new Classe("ExempleClasse");
+        maClasse.ajouterAttribut("attribut1");
+        maClasse.ajouterAttribut("attribut2");
+        maClasse.ajouterMethode("methode1");
+        maClasse.ajouterMethode("methode2");
+
+        Vue vue = new VueClasse(maClasse);
+        vue.actualiser(null);
         StackPane root = new StackPane();
+        root.getChildren().add((Label)vue);
         Scene scene = new Scene(root, 800, 600);
+
+        scene.getStylesheets().add(getClass().getResource("/style.css").toExternalForm());
         stage.setScene(scene);
         stage.show();
     }
--- src/adg/VueClasse.java
+++ src/adg/VueClasse.java
@@ -1,12 +1,27 @@
 package adg;
 
-public class VueClasse implements Vue{
+import javafx.scene.control.Label;
+
+import java.util.ArrayList;
+
+public class VueClasse extends Label implements Vue{
     private Classe classe;
     public VueClasse(Classe classe){
         this.classe = classe;
+        this.setId("classe-label");
     }
     @Override
     public void actualiser(Sujet mod) {
-        
+        ModelUML m = (ModelUML) mod;
+        String text = classe.getNom()+"\n";
+        ArrayList attributs = classe.getAttributs();
+        ArrayList methodes = classe.getMethodes();
+        for(int i = 0; i < attributs.size(); i++){
+            text+=attributs.get(i)+"\n";
+        }
+        for(int i = 0; i < methodes.size(); i++){
+            text+=methodes.get(i)+"\n";
+        }
+        this.setText(text);
     }
 }
