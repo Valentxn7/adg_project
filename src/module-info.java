8a6ae59116048ada4c0dbdf1011b07f8fa1ca118
SolareFlame <zeldac332@gmail.com>
2024-12-18 11:21:25 +0100

    📝 Update ModuleInfo (added Gson for Json)

--- src/module-info.java
+++ src/module-info.java
@@ -1,6 +1,9 @@
 module org.example.adg_projet {
     requires javafx.controls;
     requires org.objectweb.asm;
+    requires com.google.gson;
 
     exports adg;
+
+    opens adg to com.google.gson;
 }
