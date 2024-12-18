c5188e673899f98588e588a308cef78db88ad9dd
Yeahboys04 <ryankorban2004@gmail.com>
2024-12-18 17:47:38 +0100

    Mise a jour du module-info.java

--- src/module-info.java
+++ src/module-info.java
@@ -2,7 +2,7 @@ module org.example.adg_projet {
     requires javafx.controls;
     requires com.google.gson;
 
+    opens adg to com.google.gson;
     exports adg;
 
-    opens adg to com.google.gson;
 }
