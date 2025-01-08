67ff0b67d1274fd6f1efd978017934b55ea4b815
Valentxn7 <gugug3293@gmail.com>
2025-01-08 11:24:38 +0100

    fix titre

--- src/adg/ModelUML.java
+++ src/adg/ModelUML.java
@@ -160,7 +160,6 @@ public class ModelUML implements Sujet {
     /**
      * Crée un projet vierge, survient quand on clique sur Nouveau Projet
      *
-     * @param nomProjet nom du projet
      * @return true si le projet a été créé, false sinon
      */
     private void ajouterFlecheImp(Classe classe, VueClasse vueClasse) {
@@ -359,7 +358,7 @@ public class ModelUML implements Sujet {
      */
     public void setWindowsTitle(String titre) {
         this.windowsTitle = titre;
-        this.stage.setTitle(titre);
+        this.stage.setTitle(getWindowsTitle());
     }
 
     public ArrayList<Classe> getClasses() {
