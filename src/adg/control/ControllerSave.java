4a3ac76b23ed75ae3de50b526082f0fa779a06ad
SolareFlame <zeldac332@gmail.com>
2025-01-09 09:32:08 +0100

    🚀  New Save option

---
+++ src/adg/control/ControllerSave.java
@@ -0,0 +1,33 @@
+package adg.control;
+
+import adg.MainUML;
+import adg.ModelUML;
+import adg.data.Export;
+import adg.data.Save;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.TextField;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+
+public class ControllerSave implements EventHandler<ActionEvent> {
+    private ModelUML modelUML;
+    private Stage rootStage;
+
+    public ControllerSave(ModelUML modelUML, Stage rootStage) {
+        this.modelUML = modelUML;
+        this.rootStage = rootStage;
+    }
+
+    public void handle(ActionEvent event) {
+        String path = modelUML.getFolderPath();
+
+
+        Save.save(modelUML.getClasses(), path);
+
+        System.out.println("Enregistrer vers: " + path);
+    }
+}
